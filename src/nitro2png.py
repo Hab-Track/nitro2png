229603ea8b06e4d4b854d69bb18dda2a0307c6bb
Lande <laande.pro@gmail.com>
2026-01-30 17:15:03 +0100

    handle raw png in nitro file

--- src/nitro2png.py
+++ src/nitro2png.py
@@ -16,45 +16,62 @@ def is_probably_reversed(data: bytes) -> bool:
     return False
 
 
+def is_png(data: bytes) -> bool:
+    png_signature = b'\x89PNG\r\n\x1a\n'
+    return data.startswith(png_signature)
+
+
+def extract_zip(file):
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        for file_info in zip_ref.infolist():
+            with zip_ref.open(file_info) as f:
+                data = f.read()
+                yield file_info.filename, data
+
+
+def unpack_nitro(data: bytes):
+    read_bytes = ReadBytes(data)
+
+    file_count = read_bytes.read_short()
+    if file_count <= 0:
+        file_count = read_bytes.read_short()
+
+    for _ in range(file_count):
+        name = read_bytes.read_string()
+        length = read_bytes.read_int()
+        compressed = read_bytes.read_bytes(length)
+        if compressed.startswith(b'x\x9c'):
+            decompressor = zlib.decompressobj()
+        elif compressed.startswith(b'\x1f\x8b\x08'):
+            decompressor = zlib.decompressobj(16 + zlib.MAX_WBITS)
+        else:
+            print(f"Unknown compression format for {name}")
+            continue
+
+        try:
+            decompressed = decompressor.decompress(compressed) + decompressor.flush()
+
+            if is_probably_reversed(decompressed):
+                decompressed = decompressed[::-1]
+
+            yield name, decompressed
+
+        except zlib.error as e:
+            print(f"Error decompressing {name}: {e}")
+
+
 def convert(file):
     try:
-        with zipfile.ZipFile(file, 'r') as zip_ref:
-            for file_info in zip_ref.infolist():
-                with zip_ref.open(file_info) as f:
-                    data = f.read()
-                    yield file_info.filename, data
+        yield from extract_zip(file)
     except zipfile.BadZipFile:
         with open(file, 'rb') as f:
             data = f.read()
-
-        read_bytes = ReadBytes(data)
-
-        file_count = read_bytes.read_short()
-        if file_count <= 0:
-            file_count = read_bytes.read_short()
-
-        for _ in range(file_count):
-            name = read_bytes.read_string()
-            length = read_bytes.read_int()
-            compressed = read_bytes.read_bytes(length)
-            if compressed.startswith(b'x\x9c'):
-                decompressor = zlib.decompressobj()
-            elif compressed.startswith(b'\x1f\x8b\x08'):
-                decompressor = zlib.decompressobj(16 + zlib.MAX_WBITS)
-            else:
-                print(f"Unkwown compression format for {name}")
-                continue
-
-            try:
-                decompressed = decompressor.decompress(compressed) + decompressor.flush()
-
-                if is_probably_reversed(decompressed):
-                    decompressed = decompressed[::-1]
-
-                yield name, decompressed
-
-            except zlib.error as e:
-                print(f"Error decompressing {name}: {e}")
+        
+        if is_png(data):
+            file = file.replace('.nitro', '.png')
+            yield file, data
+        else:
+            yield from unpack_nitro(data)
 
 
 def save(file, r):
